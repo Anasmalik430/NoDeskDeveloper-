e26eb5624974629c8e2f4383637aa52f2985d622
Anasmalik57 <itsanas474@gmail.com>
2025-11-27 18:37:24 +0530

    folow cursor modified

--- components/MiniComponents/FollowCursor.js
+++ components/MiniComponents/FollowCursor.js
@@ -1,5 +1,6 @@
 "use client";
 import { useMotionValue, useSpring, motion } from "framer-motion";
+import { Code2 } from "lucide-react";
 import { useEffect } from "react";
 const FollowCursor = () => {
   const cursorX = useMotionValue(-100);
@@ -20,9 +21,11 @@ const FollowCursor = () => {
     <>
       {/* Magical Cursor */}
       <motion.div
-        className="fixed top-0 left-0 size-12 border-2  border-white  bg-radial-[at_25%_25%] from-blue-400 to-emerald-900 to-75%  rounded-full pointer-events-none z-50 mix-blend-screen opacity-70 blur-xs cursor-pointer "
+        className="fixed top-0 left-0 size-12 border border-white/20 shadow-xl shadow-zinc-900 grid place-items-center bg-radial-[at_25%_25%] from-white to-zinc-900 to-55%  rounded-full pointer-events-none z-50 mix-blend-screen  cursor-pointer "
         style={{ translateX: cursorXSpring, translateY: cursorYSpring }}
-      />
+      >
+        <Code2 color="#ffff" className="bg-linear-to-tl from-blue-400 via-sky-400 to-teal-500 text-transparent bg-clip-text"/>
+      </motion.div>
     </>
   );
 };
