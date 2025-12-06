672c9f065a3257a9eee5a5716f5237768d3ca724
Anasmalik57 <itsanas474@gmail.com>
2025-12-06 10:23:00 +0530

    minor changes on bottomctaButton.js

--- components/MiniComponents/BottomCTAButton.js
+++ components/MiniComponents/BottomCTAButton.js
@@ -9,16 +9,15 @@ const BottomCTAButton = () => {
       <div className="mt-20 relative">
         <div className="absolute hidden inset-0 bg-linear-to-r from-blue-500 via-sky-500 to-teal-500 rounded-3xl blur-2xl opacity-20" />
         <div className="relative bg-linear-to-r from-white/10 to-white/5 backdrop-blur-xl border border-blue-500/30 rounded-3xl p-12 text-center">
-          <h2 className="text-xl md:text-3xl font-bold text-white mb-4">
+          <h2 className="text-xl md:text-4xl font-bold text-white mb-4">
             Need a Custom Solution?
           </h2>
-          <p className="text-gray-400 mb-8 max-w-2xl mx-auto text-[11px]">
+          <p className="text-gray-400 mb-8 max-w-2xl mx-auto text-[12px]">
             Looking for something specific? Our team can build custom solutions
             tailored to your exact requirements. Get in touch for a personalized
             quote.
           </p>
-          <Link href={"https://api.whatsapp.com/send?phone=+918121923831&text=Hello%NoDeskDev%Team,%20I%20need%20your%20service"} className="hidden md:block group relative px-10 py-5 bg-linear-to-r from-blue-600 via-sky-500 to-teal-400 rounded-2xl font-bold text-white overflow-hidden shadow-2xl hover:shadow-sky-500/70 transition-all duration-400 hover:scale-105 active:scale-95" target="_blank">
-            <span className="absolute inset-0 bg-white/20 -translate-x-full group-hover:translate-x-full transition-transform duration-1000" />
+          <Link href={"https://api.whatsapp.com/send?phone=+918121923831&text=Hello%NoDeskDev%Team,%20I%20need%20your%20service"} className="hidden md:block group relative px-10 py-5 transition-all duration-400 hover:scale-105 active:scale-95 border w-fit  border-white/25 rounded-full" target="_blank">
             <span className="relative flex items-center gap-2 cursor-pointer">
               <Sparkles className="w-5 h-5" />
               <span className="hidden sm:block">Get Custom Quote </span>
@@ -26,7 +25,7 @@ const BottomCTAButton = () => {
               <ArrowRight className="w-5 h-5 group-hover:translate-x-1 transition-transform duration-300" />
             </span>
           </Link>
-          <Link href={"https://api.whatsapp.com/send?phone=+918121923831&text=Hello%NoDeskDev%Team,%20I%20need%20your%20service"} className="group relative flex justify-center items-center gap-2 cursor-pointer" target="_blank">
+          <Link href={"https://api.whatsapp.com/send?phone=+918121923831&text=Hello%NoDeskDev%Team,%20I%20need%20your%20service"} className="md:hidden group relative flex justify-center items-center gap-2 cursor-pointer" target="_blank">
           <span className="border flex gap-2 items-center px-5 py-2 rounded-full border-white/25 bg-white/8">
 
               <Sparkles className="w-5 h-5" />
